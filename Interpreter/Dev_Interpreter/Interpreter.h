caa1e1178f4233f7007d8b66fb321305ce8ea4e5
hGriff0n <ghooper96@gmail.com>
2015-06-27 21:45:10 -0400

    Development System Plan
    Settled on a general plan for how to begin implementing dust. The basic
    idea is to work on the interpreter as-if it were a compiler (build a
    lexer, then a compiler, than a vm (possibly) before combining the stages
    into the interpreter). The idea is that by doing so, I will gradually
    improve the efficiency of each phase (in ways that can benefit the whole
    program) while also finding and ironing out kinks and possible undefined
    behaviors at the earliest possible moment (where fixing such problems is
    the easiest).
    Reorganized the file structure to reflect the new plan

---
+++ Interpreter/Dev_Interpreter/Interpreter.h
@@ -0,0 +1,5 @@
+#pragma once
+
+class DustInterpreter {					// This is a very basic interpreter. This is not the final version that will be integrated into the public api
+
+};
---
+++ Interpreter/Dev_Lexer/Lexer.h
@@ -0,0 +1,5 @@
+#pragma once
+
+class DustLexer {		// Should I create a generalized log system for testing ???
+
+};
