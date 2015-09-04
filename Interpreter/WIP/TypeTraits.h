b0cf15749476b7a9f9487869b05ec43616ceaa90
hGriff0n <ghooper96@gmail.com>
2015-09-04 17:50:05 -0400

    Notes and Comments
    Added a Converter/Conversions section to TypeNotes.txt (Currently
    unfinished)
    Modified the comments in TypeTraits.h
    Added a section detailing current problems with implementing functions
    and tables

--- Interpreter/WIP/TypeTraits.h
+++ Interpreter/WIP/TypeTraits.h
@@ -1,7 +1,14 @@
 #pragma once
 
 #include "Value.h"
+//#include "GC.h"				// Wouldn't need to include <string>
 #include <string>
+
+// TypeTraits is currently only referenced in CallStack.h, Stack.h (id only), Init.cpp (id only), and testing.cpp
+	// Should I include "GC.h" here I can move the explicit specializations for get and the string specialization for make into this file
+	// Should reduce possibilities of future errors (though I still need to find out how the error occurred (for extensibility))
+
+// Perhaps move TypeTraits into dust::impl
 
 namespace dust {
 	namespace impl {
@@ -11,17 +18,17 @@ namespace dust {
 
 template <typename T>
 struct TypeTraits{
+	// 
 	static size_t id;
 
+	// Convert an object of type T into its dust representation
 	static dust::impl::Value make(T v, dust::impl::GC& gc) {
 		return{ v, TypeTraits<T>::id };
 	}
 
+	// Create an object of type T from its dust representation
 	static T get(const dust::impl::Value&, dust::impl::GC&);
 };
 
-// std::string has a different implementation of make (currently defined in CallStack.h)
-// However the std::specialization for TypeTraits::make is being created somewhere between here and there
-	// Giving TypeTraits<std::string> the wrong make function. This alleviates that error at the cost of a C4506 warning
-//template<> dust::impl::Value TypeTraits<std::string>::make(std::string, dust::impl::GC&);
+
 template <typename T> size_t TypeTraits<T>::id = -1;
