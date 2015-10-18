07bb84ac885af8ba161500f1e60c7631c6036056
hGriff0n <ghooper96@gmail.com>
2015-10-18 13:35:51 -0400

    Changed com and findDef implementation
    Changed com to return Nil/Table when one of the type arguments is of the
    respective types
    Changed findDef to first search in the Bool type if its type argument is
    Nil (for _op= and _ou!)

--- Interpreter/TypeSystem.h
+++ Interpreter/TypeSystem.h
@@ -73,7 +73,8 @@ namespace dust {
 				static const size_t NO_DEF = -1;
 
 				// Inheritance Resolution (returns NO_DEF if no definition is found)
-				// Find op definition in type or in parent(type)
+				// Find a definition for the function in type or ancestors(type)
+				// Returns findDef(Traits<bool>::id, fn) if t == Traits<Nil>::id
 				size_t findDef(size_t t, std::string fn);
 
 
@@ -83,6 +84,7 @@ namespace dust {
 
 
 				// Common Type Resolution (Find a type that defines op and that both l and r can be cast to)
+				// Returns Nil or Table if l or r are of the given types
 				size_t com(size_t l, size_t r, std::string op);
 				size_t com(Type& l, Type& r, std::string op);
 
